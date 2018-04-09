58726a11183071e308acf16dbce1798a6e29ca86
MariajMesa <36260835+MariajMesa@users.noreply.github.com>
2018-04-09 12:50:03 +0200

    Update Toponimos_install.sh

--- Toponimos_install.sh
+++ Toponimos_install.sh
@@ -4,4 +4,4 @@
 sudo git clone hhtps://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
 
 # 2.-Acceso directo
-sudo ln -s /opt/odoo/l10n-spain//l10n_es_toponyms /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons

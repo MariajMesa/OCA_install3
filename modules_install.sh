2c68e6e084702ce4a2345d009156ec6e7b5dbc0f
MariajMesa <36260835+MariajMesa@users.noreply.github.com>
2018-04-11 11:56:27 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -47,7 +47,7 @@ sudo ln -s $OE_HOME/bank-payment/account_banking_mandate/ $OE_HOME/custom/addons
 sudo ln -s $OE_HOME/bank-payment/account_banking_pain_base/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_direct_debit/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_payment_partner $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-statement-import/account_bank_statement_import_43/ $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/bank-statement-import/l10n_es_account_bank_statement_import_n43/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachment_preview/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachments_to_filesystem/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/document_page/ $OE_HOME/custom/addons/
